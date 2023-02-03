6cea6335c49f0a079965e73b059fa6934d6619df
GitHridoy <mdhridoy10000000@gmail.com>
2023-02-03 12:54:58 +0600

    first

---
+++ var-names.js
@@ -0,0 +1,19 @@
+var price = 29;
+
+
+
+//right var names can be store
+var home_address = "Faridpur-Bhanga";
+var home65 = "London-Pricess";
+
+
+//right var name some structure
+var vilage_address = "Borisal";//snake case
+var hommeAddressFirst = "chitagang"; //camel case
+var HomeAddressLine = "Jamalpur"; //pascal case
+
+
+//var same name but key sansitive to separate 
+var price = 33;
+var Price = 32;
+
---
+++ var.js
@@ -0,0 +1,17 @@
+
+//Variable Number Type
+var price = 20;
+var age = 44;
+var temp = 11;
+
+
+//Variable String Type
+var name = "Hridoy Vai";
+var location = "Faridpur Bhanga";
+var subject = "Islam";
+
+
+//Boolean
+var hacks = true;
+var recover = true;
+var apeal = true;
