7b95cfdd5d46410c40b62197ddf013b5c713030f
GitHridoy <mdhridoy10000000@gmail.com>
2023-02-04 11:49:28 +0600

    fir

---
+++ operation.js
@@ -0,0 +1,28 @@
+// var jog biog korte hoy jevabe
+var onionPrice = 70;
+var eggPrice = 40;
+var TotalPrice = onionPrice + eggPrice;
+var pricedifarance = onionPrice - eggPrice;
+console.log(pricedifarance);
+
+
+// var gun korte hoy jevabe
+var orangePrice = 70;
+var quantity = 9;
+var totalcost = orangePrice * quantity;
+console.log(totalcost);
+
+
+// var vag korte hoy javabe
+var mony = 100;
+var man = 5;
+var vag = mony / man;
+console.log(vag);
+
+
+// var declaration
+var tomatoprice = 25;
+// var value update
+tomatoprice = 30;
+
+
