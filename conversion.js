3147c3cb5a54d3f3a95eda7224b632f46f209ab3
GitHridoy <mdhridoy10000000@gmail.com>
2023-02-05 17:21:50 +0600

    conver

---
+++ conversion.js
@@ -0,0 +1,10 @@
+var price1 = 40;
+var price2 = 50;
+var totalprice = price1 + price2;
+// console.log(totalprice);
+
+var firstname = 'hridoy';
+var lastname = 'zohirul';
+var fullname = firstname + '-' + lastname;
+console.log(fullname);
+
