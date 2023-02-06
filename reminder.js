8eb39d75f827281b6f37f2e63353fcd693949e63
GitHridoy <mdhridoy10000000@gmail.com>
2023-02-06 17:31:01 +0600

    vag

---
+++ reminder.js
@@ -0,0 +1,5 @@
+var mangoes = 101;
+var person =5;
+// / this means vagfol
+// % this means vagses
+console.log(mangoes%person);
---
+++ vartype.js
@@ -0,0 +1,12 @@
+var aluprice = 30;
+var name = 'Hridoy';
+var Hacks = true;
+var love;
+console.log(typeof love);
+
+// fixed some js output serial
+var num1 = 0.1;
+var num2 = 0.6;
+var numtotal = num1 + num2;
+numtotal = numtotal.toFixed(1);
+console.log(numtotal);
