796a258dfb1bcf4dbbb0ba0ae0503e757f9f212e
GitHridoy <mdhridoy10000000@gmail.com>
2023-02-12 19:43:02 +0600

    num

---
+++ extra.js
@@ -0,0 +1,8 @@
+var num1 = 90;
+var num2 = 90;
+
+// console.log( num1 < num2);
+console.log( num1 > num2);
+//console.log( num1 === num2);
+//console.log( num1 !== num2);
+//console.log( num1 >= num2);
