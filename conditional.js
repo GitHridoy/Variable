eba81853acc81322bd9e742abd2489fa45ba998a
GitHridoy <mdhridoy10000000@gmail.com>
2023-02-13 19:55:20 +0600

    con

---
+++ conditional.js
@@ -0,0 +1,17 @@
+var iphonePrice = 79000;
+var Buget = 95000;
+
+// এটা লেস দেন মানে হল সংখা ছোট হতে হবে
+if (iphonePrice < Buget){
+    console.log('i phone kine futani marbo');
+}
+
+
+var football = 320;
+var mony = 320;
+
+// এটা লেস দেন ইকুয়াল মানে হল ছোট হোক অথবা সমান
+if (football <= mony){
+    console.log('Eta Amar Football');
+}
+     
--- extra.js
+++ extra.js
@@ -1,8 +1,13 @@
-var num1 = 90;
+var num1 = 80;
 var num2 = 90;
 
 // console.log( num1 < num2);
-console.log( num1 > num2);
+// console.log( num1 > num2);
 //console.log( num1 === num2);
 //console.log( num1 !== num2);
 //console.log( num1 >= num2);
+//console.log( num1 <= num2);
+
+// money1 > money2 && result1 > result2 && height1 > height2
+
+money1 > money2 || result1 > result2 || height1 > height2
