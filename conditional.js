8b0f5530085070316628a1d6cb829455f2406f1e
GitHridoy <mdhridoy10000000@gmail.com>
2023-02-14 18:36:54 +0600

    else

--- conditional.js
+++ conditional.js
@@ -14,4 +14,60 @@ var mony = 320;
 if (football <= mony){
     console.log('Eta Amar Football');
 }
+
+
+// ইফ এলস কন্ডিসন এভাবে সেট করতে হয়
+var Ruti = 10;
+var Mony = 5;
+
+if (Ruti < Mony){
+    console.log('Ji Vai Ami Ruti Khamu');
+}
+else{
+    console.log('Na vai amar Baget Nai');
+}
+
+
+// এটা দিয়ে মাল্টিপল কন্ডিসন সেট করে যায়
+var isGraduated = true;
+var sallery = 60000;
+var cars = 4;
+if (isGraduated == true && sallery > 50000 && cars <= 3){
+    console.log('Aso Bia Kori')
+}
+else{
+    console.log('Tumi Joggo Patro Na')
+}
+
+
+// এটা দিয়ে মাল্টিপল কন্ডিসন সেট করে যায় এবং কিছু কন্ডিসন ফলস হলেও পাস করায় দিবে
+var Home = true;
+var Taka = 40000;
+var Hunda = true;
+
+if (Home == true || Taka > 50000 || Hunda == true){
+    console.log('Aso Bia Kori')
+}
+else{
+    console.log('Tumi Joggo Patro Na')
+}
+
+
+
+
+// এটা ইফ এলস কন্ডিসন চেক করে যদি সঠিক পায় তাহলে ওই কন্ডিসনেই ব্রেক হয়ে যাবে আর সঠিক না পেলে নি নিচের কন্ডিসন গুলো চেক করবে
+var mony = 15;
+var mamabiskut = 30;
+var coko = 90;
+var losen = 15;
+
+if (mamabiskut < mony){
+    console.log('Mama Biskut Khamu')
+}
+else if (coko < mony){
+    console.log('Ami Coko Khamu')
+}
+else if(losen <= mony){
+    console.log("ami losen khamu")
+}
      
--- extra.js
+++ extra.js
@@ -10,4 +10,22 @@ var num2 = 90;
 
 // money1 > money2 && result1 > result2 && height1 > height2
 
-money1 > money2 || result1 > result2 || height1 > height2
+// money1 > money2 || result1 > result2 || height1 > height2
+
+
+
+// এটা ইফ এলস কন্ডিসন চেক করে যদি সঠিক পায় তাহলে ওই কন্ডিসনেই ব্রেক হয়ে যাবে আর সঠিক না পেলে নি নিচের কন্ডিসন গুলো চেক করবে
+var mony = 15;
+var mamabiskut = 30;
+var coko = 90;
+var losen = 15;
+
+if (mamabiskut < mony){
+    console.log('Mama Biskut Khamu')
+}
+else if (coko < mony){
+    console.log('Ami Coko Khamu')
+}
+else if(losen <= mony){
+    console.log("ami losen khamu")
+}
