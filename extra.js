fbd43145373a8500577a41e57e929b53e0e45e13
GitHridoy <mdhridoy10000000@gmail.com>
2023-02-19 15:25:14 +0600

    while

--- extra.js
+++ extra.js
@@ -1,31 +1,6 @@
-var num1 = 80;
-var num2 = 90;
-
-// console.log( num1 < num2);
-// console.log( num1 > num2);
-//console.log( num1 === num2);
-//console.log( num1 !== num2);
-//console.log( num1 >= num2);
-//console.log( num1 <= num2);
-
-// money1 > money2 && result1 > result2 && height1 > height2
-
-// money1 > money2 || result1 > result2 || height1 > height2
-
-
-
-// এটা ইফ এলস কন্ডিসন চেক করে যদি সঠিক পায় তাহলে ওই কন্ডিসনেই ব্রেক হয়ে যাবে আর সঠিক না পেলে নি নিচের কন্ডিসন গুলো চেক করবে
-var mony = 15;
-var mamabiskut = 30;
-var coko = 90;
-var losen = 15;
-
-if (mamabiskut < mony){
-    console.log('Mama Biskut Khamu')
-}
-else if (coko < mony){
-    console.log('Ami Coko Khamu')
-}
-else if(losen <= mony){
-    console.log("ami losen khamu")
+var Rost_Daw = 0;
+while(Rost_Daw < 7){
+    console.log('Taderke 7 Ta Rost Den')
+    Rost_Daw++;
+    
 }
