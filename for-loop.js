75445c560c2ed2d0a925b06fb261db653c23a1e2
GitHridoy <mdhridoy10000000@gmail.com>
2023-02-20 20:10:00 +0600

    for

--- for-loop.js
+++ for-loop.js
@@ -1,10 +1,4 @@
-var ruti = 0;
-while(ruti <= 13){
-    console.log(ruti)
-    ruti = ruti + 3;
-}
-
 // এটা ফর লুপ এবং এর ভিতরে ভেরিয়েবল সহ সবকিছু রাখা যায় 
-for(var Puri = 0; Puri < 100; Puri++){
-    console.log(Puri);
+for (var i = 0; i < 10; i++){
+    console.log(i);
 }
