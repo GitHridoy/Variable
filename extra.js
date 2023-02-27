f7037e141ca6b31dafa970ec3129dc4f809ada69
GitHridoy <mdhridoy10000000@gmail.com>
2023-02-27 11:04:26 +0600

    forloop

--- extra.js
+++ extra.js
@@ -1,6 +1,13 @@
+// // এটা ফর লুপ এবং একটি ভেরিয়েবল এর ভিতরের এলিমেন্ট কে বের করা সম্বব
+// var numbers = [33, 44, 11, 13, 10, 18, 19, 22];
+// for(var myindex = 0; myindex < 8; myindex++){
+//     var findenumbers = numbers[myindex];
+//     console.log(findenumbers)
+// }
 
-var number = 0;
-while (number <= 10){
-    console.log(number)
-    number++;
+
+var numbers = [33, 44, 11, 13, 10, 18, 19, 22, 42, 100];
+for(var myindex = 0; myindex < numbers.length; myindex++){
+    var findenumbers = numbers[myindex];
+    console.log(findenumbers)
 }
--- for-loop.js
+++ for-loop.js
@@ -2,3 +2,18 @@
 for (var i = 0; i < 10; i++){
     console.log(i);
 }
+
+// এটা ফর লুপ এবং একটি ভেরিয়েবল এর ভিতরের এলিমেন্ট কে বের করা সম্বব
+var numbers = [33, 44, 11, 13, 10, 18, 19, 22];
+for(var myindex = 0; myindex < 8; myindex++){
+    var findenumbers = numbers[myindex];
+    console.log(findenumbers)
+}
+
+
+// এটা ফর লুপ এবং একটি ভেরিয়েবল এর ভিতরের সব এলিমেন্ট কে বের করা যায়
+var numbers = [33, 44, 11, 13, 10, 18, 19, 22, 42, 100];
+for(var myindex = 0; myindex < numbers.length; myindex++){
+    var findenumbers = numbers[myindex];
+    console.log(findenumbers)
+}
