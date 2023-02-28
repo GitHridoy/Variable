1c7afb2f881192330c452671934165adeb9977ec
GitHridoy <mdhridoy10000000@gmail.com>
2023-02-28 15:37:34 +0600

    pu

---
+++ brake.js
@@ -0,0 +1,7 @@
+// এটা ফর লুপ সাথে ব্রেক কন্ডিসন মানে হল কোনো নিদ্রিস্ট সংখা চালিয়ে বন্ধ হয়ে যাবে
+for(var gift = 1; gift < 15; gift++){
+    console.log('Amake Kichu gift Daw');
+    if(gift >= 5){
+        break;
+    }
+}
