97fc00708453082517478bf90525207133bb60b0
GitHridoy <mdhridoy10000000@gmail.com>
2023-03-01 16:24:09 +0600

    brake

--- for-loop.js
+++ for-loop.js
@@ -17,3 +17,14 @@ for(var myindex = 0; myindex < numbers.length; myindex++){
     var findenumbers = numbers[myindex];
     console.log(findenumbers)
 }
+
+
+// এটা ফর লুপ এবং একটি ভেরিয়েবল এর ভিতরের সব এলিমেন্ট কে বের করা যায় এবং চাইলে নিদ্রিস্ট ইন্ডেক্সে ব্রেক ও করা যায়
+var model = ['w68', 'w45', 'v75', 'v85', 'v95', 'v45'];
+for(var is = 0; is < model.length; is++){
+    var finde = model[is];
+    if(finde == 'v95'){
+        break;
+    }
+    console.log(finde)
+}
--- while.js
+++ while.js
@@ -6,3 +6,13 @@ while(Rost_Daw < 7){
     Rost_Daw++;
     
 } 
+
+// এটা হোয়াইল লুপ একই ভেরিয়েবলের মান বার বার সেট করা যায় এবং ব্রেক ও করা যায়
+var mobile = 1;
+while(mobile < 10){
+    console.log('Amake 10 ta mobile den');
+    mobile++
+    if(mobile > 7){
+        break;
+    }
+}
